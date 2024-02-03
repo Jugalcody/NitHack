d371ba902b49735bc408aaa8f2748eb8088e06f0
Jugalcody <jugalnerist@gmail.com>
2024-02-03 12:50:14 +0530

    modified a bit

--- sahaya/app/src/main/java/com/example/sahaya/Security.java
+++ sahaya/app/src/main/java/com/example/sahaya/Security.java
@@ -1,14 +1,74 @@
 package com.example.sahaya;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Security extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView nav;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security);
+        drawerLayout = findViewById(R.id.drawerLayout_security);
+        toolbar = findViewById(R.id.toolbar_security);
+        nav = findViewById(R.id.navigationview_drawer_security);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_profile) {
+
+                    Toast.makeText(getApplicationContext(), "profile", Toast.LENGTH_SHORT).show();
+                }
+                else if (itemId==R.id.nav_home) {
+
+                }
+                else if(itemId==R.id.nav_QRcode){
+
+                }else if(itemId==R.id.nav_setting) {
+
+
+
+                }
+                else if (itemId==R.id.nav_logout) {
+                    Intent i=new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    Toast.makeText(getApplicationContext(), "logging out", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+
+        }
+    }
+
 }
